bd42308d3c1a776c82f6b27cfc38edf424fd22af
think2011 <452125301.hzplay@gmail.com>
2017-04-16 05:02:09 +0800

    feat 🌟 deploy

--- deploy.sh
+++ deploy.sh
@@ -1,9 +1,18 @@
 HOST=root@ss.think2011.net
 SCRIPTS="
     cd apps/messager;
+
+    echo '*** 获取代码 ***';
     git pull;
+    echo;
+
+    echo '*** 更新依赖 ***';
     yarn;
-    pm2 reload;
+    echo;
+
+    echo '*** 重启服务 ***';
+    pm2 reload messager;
+    echo;
 "
 
 ssh ${HOST} ${SCRIPTS}
