044c0e0302b238a8b8cde7463dcc5c3888f77cfe
think2011 <452125301.hzplay@gmail.com>
2017-05-01 21:26:18 +0800

    refactor 🔧 更换授权方式

--- conf/index.js
+++ conf/index.js
@@ -1,27 +1,16 @@
-const program = require('commander')
-
-program
-    .option('-s, --service [service]', '已知的邮件服务 http://nodemailer.com/smtp/well-known', 'qq')
-    .option('-u, --user [user]', '邮箱账号', '452125301@qq.com')
-    .option('-p, --pass [pass]', '邮箱密码', null)
-    .option('-f, --from [from]', '发件人', '452125301@qq.com')
-    .option('-t, --to [to]', '收件人', '452125301@qq.com')
-    .option('--vultrUser [vultrUser]', 'vultr账号', '452125301.hzplay@gmail.com')
-    .option('--vultrPass [vultrPass]', 'vultr密码', null)
-    .parse(process.argv)
-
+const {email, vultr} = require('./accounts')
 
 let conf = {
     email  : {
-        service: program.service,
-        user   : program.user,
-        pass   : program.pass,
-        from   : program.from,
-        to     : program.to
+        service: 'qq',
+        user   : email.user,
+        pass   : email.pass,
+        from   : '452125301@qq.com',
+        to     : '452125301@qq.com'
     },
     vultr  : {
-        user: program.vultrUser,
-        pass: program.vultrPass
+        user: vultr.user,
+        pass: vultr.pass
     },
     headers: {
         'User-Agent': 'Mozilla/5.0 (iPhone; CPU iPhone OS 9_1 like Mac OS X) AppleWebKit/601.1.46 (KHTML, like Gecko) Version/9.0 Mobile/13B143 Safari/601.1',
